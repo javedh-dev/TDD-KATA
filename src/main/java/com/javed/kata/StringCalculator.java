baa7f5c21ae819781fca05c2efa29b0bc23d9bbf
Javed Hussain <javedh.dev@gmail.comjaved>
2021-02-13 17:51:35 +0530

    Refactored code to support custom delimiter.

--- src/main/java/com/javed/kata/StringCalculator.java
+++ src/main/java/com/javed/kata/StringCalculator.java
@@ -5,10 +5,22 @@ public class StringCalculator {
     public int add(String numbers){
         int sum = 0;
         if(!numbers.isEmpty()) {
-            String[] nums = numbers.split("[,\n]");
-            for(String num : nums){
-                sum+=Integer.parseInt(num);
+            if(numbers.startsWith("//")){
+                String[] temp = numbers.split("\n",2);
+                String del = temp[0].substring(2);
+                sum = getSum(temp[1],del);
+            }else {
+                sum = getSum(numbers, "[,\n]");
             }
+        }
+        return sum;
+    }
+
+    private int getSum(String numbers, String del) {
+        int sum = 0;
+        String[] nums = numbers.split(del);
+        for (String num : nums) {
+            sum += Integer.parseInt(num);
         }
         return sum;
     }
