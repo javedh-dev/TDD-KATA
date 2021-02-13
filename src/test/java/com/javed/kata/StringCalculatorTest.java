d147ab9e2d3d9c5f42b86c2c35483b59921c239a
Javed Hussain <javedh.dev@gmail.comjaved>
2021-02-13 17:13:46 +0530

    Created test for single number.

--- src/test/java/com/javed/kata/StringCalculatorTest.java
+++ src/test/java/com/javed/kata/StringCalculatorTest.java
@@ -1,13 +1,22 @@
 package com.javed.kata;
 
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 
 public class StringCalculatorTest {
 
+    private final StringCalculator calculator = new StringCalculator();
+
+    @DisplayName("Empty string returned 0")
     @Test
     public void testEmptyString(){
-        StringCalculator calculator = new StringCalculator();
         assert calculator.add("")==0;
+    }
+
+    @DisplayName("One number is returned as it is.")
+    @Test
+    public void testOneNumber(){
+        assert calculator.add("4")==4;
     }
 
 }
