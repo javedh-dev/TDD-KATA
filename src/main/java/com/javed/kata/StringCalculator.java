31963f741fa1e4402c4bb0821c95107ffd6728da
Javed Hussain <javedh.dev@gmail.comjaved>
2021-02-13 17:25:57 +0530

    Refactored code to add multiple numbers

--- src/main/java/com/javed/kata/StringCalculator.java
+++ src/main/java/com/javed/kata/StringCalculator.java
@@ -6,10 +6,8 @@ public class StringCalculator {
         int sum = 0;
         if(!numbers.isEmpty()) {
             String[] nums = numbers.split(",");
-            if(nums.length==2){
-                sum = Integer.parseInt(nums[0])+Integer.parseInt(nums[1]);
-            }else {
-                sum = Integer.parseInt(nums[0]);
+            for(String num : nums){
+                sum+=Integer.parseInt(num);
             }
         }
         return sum;
